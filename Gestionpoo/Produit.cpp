228703ddc6a18e1810343c5eaebb5f37d338477d
Essaid BENAMAR <essaid.benamar.dz@viacesi.fr>
2020-11-28 15:10:08 +0100

    said ki L3ada

---
+++ Gestionpoo/Produit.cpp
@@ -0,0 +1,88 @@
+#include "Produit.h"
+
+System::String^ Produit::Getref()
+{
+	throw gcnew System::NotImplementedException();
+	// TODO: insérer une instruction return ici
+}
+
+void Produit::Setref(System::String^ ref)
+{
+	throw gcnew System::NotImplementedException();
+}
+
+System::String^ Produit::Getnom()
+{
+	throw gcnew System::NotImplementedException();
+	// TODO: insérer une instruction return ici
+}
+
+void Produit::Setnom(System::String^ nom)
+{
+	throw gcnew System::NotImplementedException();
+}
+
+float Produit::Getprix_ht()
+{
+	return 0.0f;
+}
+
+void Produit::Setprix_ht(float prix_ht)
+{
+	throw gcnew System::NotImplementedException();
+}
+
+int Produit::Gettva()
+{
+	return 0;
+}
+
+void Produit::Settva(int tva)
+{
+	throw gcnew System::NotImplementedException();
+}
+
+int Produit::Getquantite_stock()
+{
+	return 0;
+}
+
+void Produit::Setquantite_stock(int quantite_stock)
+{
+	throw gcnew System::NotImplementedException();
+}
+
+int Produit::Getquantit_reap()
+{
+	return 0;
+}
+
+void Produit::Setquantit_reap(int quantit_reap)
+{
+	throw gcnew System::NotImplementedException();
+}
+
+float Produit::Getprix_ttc()
+{
+	return 0.0f;
+}
+
+void Produit::Setprix_ttc(float prix_ttc)
+{
+	throw gcnew System::NotImplementedException();
+}
+
+Produit::Produit(System::String^ nom, System::String^ ref, float prix_ht, int tva, int quantit_stock, int quantit_reap, float prix_ttc)
+{
+	throw gcnew System::NotImplementedException();
+}
+
+Produit::Produit()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+Produit::~Produit()
+{
+	throw gcnew System::NotImplementedException();
+}
