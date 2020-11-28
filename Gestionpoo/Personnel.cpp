a0fc7ed29a5667991fd4252786368eea3716aa61
Essaid BENAMAR <essaid.benamar.dz@viacesi.fr>
2020-11-28 17:21:16 +0100

    sa5id

---
+++ Gestionpoo/Personnel.cpp
@@ -0,0 +1,37 @@
+
+#include "Personnel.h"
+
+System::DateTime Personnel::Getdate_embauche()
+{
+    return System::DateTime();
+}
+
+void Personnel::Setdate_embache(System::DateTime date_embache)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+Addr Personnel::Getaddr()
+{
+    return Addr();
+}
+
+void Personnel::Setaddr_(Addr addr)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+Personnel::Personnel(System::DateTime date_embauche, Addr addr)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+Personnel::Personnel()
+{
+    throw gcnew System::NotImplementedException();
+}
+
+Personnel::~Personnel()
+{
+    throw gcnew System::NotImplementedException();
+}
