103b95f9b29e5cf5e0b457df1683c253c091ad1e
Essaid BENAMAR <essaid.benamar.dz@viacesi.fr>
2020-11-28 16:48:22 +0100

    sa4id

---
+++ Gestionpoo/Personne.cpp
@@ -0,0 +1,46 @@
+#include "Personne.h"
+
+System::String Personne::Getnom()
+{
+    
+}
+
+void Personne::Setnom(System::String nom)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+System::String Personne::Getprenom()
+{
+    
+}
+
+void Personne::Setprenom(System::String prenom)
+{
+   
+}
+
+int Personne::Getid()
+{
+    return 0;
+}
+
+void Personne::Setid(int id)
+{
+    
+}
+
+Personne::Personne(System::String nom, System::String prenom, int id)
+{
+    
+}
+
+Personne::Personne()
+{
+   
+}
+
+Personne::~Personne()
+{
+    
+}
