c85cfec34f0973a76a29efa071dfc22d27d95e42
nazim kadouche <kadouche.n@gmail.com>
2020-11-30 18:04:00 +0100

    on avance un peu

--- Gestionpoo/Addr.h
+++ Gestionpoo/Addr.h
@@ -1,5 +1,18 @@
 #pragma once
 ref class Addr
 {
+	private: System::String^ adresse;
+		   int id_ville, id_adresse;
+		   System::String^ ville;
+	public:
+		Addr();
+		Addr(int adresse);
+		System::String^ get_adresse();
+		void set_adresse(System::String^ adresse);
+		void set_ville(System::String^ ville);
+		System::String^ get_ville();
+	protected: ~Addr() {
+		delete adresse, id_ville, id_adresse, ville;
+	}
 };
 
--- Gestionpoo/FicheClient.cpp
+++ Gestionpoo/FicheClient.cpp
@@ -1,2 +1,11 @@
 #include "FicheClient.h"
 
+System::Void Gestionpoo::FicheClient::FicheClient_Load(System::Object^ sender, System::EventArgs^ e)
+{
+    this->lblid->Text = Convert::ToString(current_client->Getid());
+    this->txtnom->Text = current_client->Getnom();
+    this->txtprenom->Text = current_client->Getprenom();
+    datenais->Value = current_client->Getdate_naissance();
+    dateachat->Value = current_client->Getdate_pr_achat();
+    for
+}
