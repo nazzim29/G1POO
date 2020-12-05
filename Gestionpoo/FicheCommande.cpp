3b61f67a1b9a20cea7d51fd6fdfa5db112c41fac
nazim kadouche <kadouche.n@gmail.com>
2020-12-05 18:51:37 +0100

    d

--- Gestionpoo/FicheCommande.cpp
+++ Gestionpoo/FicheCommande.cpp
@@ -4,7 +4,7 @@
 
 void Gestionpoo::FicheCommande::set_client(String^ cl)
 {
-    lblclient->Text = "Client : " + cl;
+    lblclient->Text = + cl;
 }
 
 void Gestionpoo::FicheCommande::set_date_livraison(DateTime^ date)
@@ -29,10 +29,10 @@ void Gestionpoo::FicheCommande::set_adresse_facturation(int i)
 
 void Gestionpoo::FicheCommande::set_adresse_list(DataTable^ ds)
 {
-    comboBox1->DataSource = ds;
+    comboBox1->DataSource = ds->Copy();
     comboBox1->ValueMember = "id_adresse";
     comboBox1->DisplayMember = "adresse";
-    comboBox2->DataSource = ds;
+    comboBox2->DataSource = ds->Copy();
     comboBox2->ValueMember = "id_adresse";
     comboBox2->DisplayMember = "adresse";
 }
@@ -40,4 +40,120 @@ void Gestionpoo::FicheCommande::set_adresse_list(DataTable^ ds)
 void Gestionpoo::FicheCommande::set_article_list(DataTable^ ds)
 {
     dataGridView1->DataSource = ds;
+}
+
+System::Collections::Generic::List<Composant::Choisir^>^ Gestionpoo::FicheCommande::get_choix()
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insérer une instruction return ici
+}
+
+void Gestionpoo::FicheCommande::set_choix(System::Collections::Generic::List<Composant::Choisir^>^ l)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+
+
+Composant::Commande^ Gestionpoo::FicheCommande::get_commande()
+{
+    Composant::Commande^ c = gcnew Composant::Commande();
+    c->set_date_livraison(this->datenais->Value);
+    c->set_date_emmision(this->dateachat->Value);
+    c->set_date_reglement_solde(DateTime::MaxValue);
+    c->set_ref(lblclient->Text->Substring(0, 2) + lblclient->Text->Substring(lblclient->Text->IndexOf(' ') + 1, 2) + DateTime::Now.Year + comboBox1->Text->Substring(comboBox1->Text->IndexOf(',') + 1, 3));
+    c->set_id_adresse_facturation(Convert::ToInt32(comboBox1->SelectedValue));
+    c->set_id_adresse_livraison(Convert::ToInt32(comboBox2->SelectedValue));
+    return c;
+}
+
+System::Void Gestionpoo::FicheCommande::button2_Click(System::Object^ sender, System::EventArgs^ e)
+{
+    Listearticle^ la = gcnew Listearticle();
+    la->dt = this->listearticle;
+    la->ShowDialog();
+    if (la->valider) {
+        textBox2->Text = la->ref;
+    }
+}
+
+System::Void Gestionpoo::FicheCommande::FicheCommande_Load(System::Object^ sender, System::EventArgs^ e)
+{
+    AutoCompleteStringCollection^ d = gcnew AutoCompleteStringCollection();
+    textBox2->AutoCompleteSource = AutoCompleteSource::CustomSource;
+    for (size_t i = 0; i < listearticle->Rows->Count; i++)
+    {
+        d->Add(listearticle->Rows[i]->ItemArray[1]->ToString());
+    }
+    textBox2->AutoCompleteCustomSource = d;
+}
+
+bool Gestionpoo::FicheCommande::existinarticle(String^ f)
+{
+    for (size_t i = 0; i < listearticle->Rows->Count; i++)
+    {
+        if (listearticle->Rows[i]->ItemArray[1]->ToString()->Equals(f)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int Gestionpoo::FicheCommande::getartindex(String^ s)
+{
+    for (size_t i = 0; i < listearticle->Rows->Count; i++)
+    {
+        if (listearticle->Rows[i]->ItemArray[1]->ToString()->Equals(s)) {
+            return i;
+        }
+    }
+}
+
+System::Void Gestionpoo::FicheCommande::button1_Click(System::Object^ sender, System::EventArgs^ e)
+{
+    if (existinarticle(textBox2->Text)) {
+        if (numericUpDown1->Value != 0) {
+            int indexart = getartindex(textBox2->Text);
+
+            if (numericUpDown1->Value > Convert::ToDecimal(Convert::ToInt32(listearticle->Rows[indexart]->ItemArray[5]->ToString()))) {
+                System::Windows::Forms::MessageBox::Show("Quantité en stock insuffisante", "Incohérance", MessageBoxButtons::OK);
+                numericUpDown1->Value = Convert::ToDecimal(Convert::ToInt32(listearticle->Rows[getartindex(textBox2->Text)]->ItemArray[5]));
+            }
+            else {
+
+                float prix_ht_unit = float::Parse(listearticle->Rows[indexart]->ItemArray[3]->ToString());
+                float remise = float::Parse(textBox4->Text) / 100;
+                float tva = float::Parse(listearticle->Rows[indexart]->ItemArray[4]->ToString()) / 100;
+                float prix_ttc = prix_ht_unit + (prix_ht_unit * (tva - remise));
+                dataGridView1->Rows->Add(listearticle->Rows[indexart]->ItemArray[1]->ToString(),
+                    listearticle->Rows[indexart]->ItemArray[2]->ToString(),
+                    listearticle->Rows[indexart]->ItemArray[7]->ToString(),
+                    listearticle->Rows[indexart]->ItemArray[3]->ToString(),
+                    listearticle->Rows[indexart]->ItemArray[4]->ToString(),
+                    numericUpDown1->Value.ToString(),
+                    prix_ttc * Convert::ToInt32(numericUpDown1->Value));
+            }
+        }
+        else {
+            System::Windows::Forms::MessageBox::Show("Vous avez oublié d'insérer la quantité!");
+        }
+    }
+    else {
+        System::Windows::Forms::MessageBox::Show("La réference n'existe pas dans le stock!!");
+    }
+}
+
+System::Void Gestionpoo::FicheCommande::textBox4_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e)
+{
+    if (e->KeyChar == ',' || e->KeyChar == '.') {
+        e->KeyChar = ',';
+    }
+    else if (!System::Char::IsDigit(e->KeyChar)) {
+        e->Handled = true;
+    }
+}
+
+System::Void Gestionpoo::FicheCommande::button4_Click(System::Object^ sender, System::EventArgs^ e)
+{
+    this->Close();
 }
--- Gestionpoo/FicheCommande.h
+++ Gestionpoo/FicheCommande.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Listearticle.h"
+#include "Commande.h"
+#include "Choisir.h"
 namespace Gestionpoo {
 
 	using namespace System;
@@ -62,12 +64,38 @@ namespace Gestionpoo {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::TextBox^ textBox2;
 	private: System::Windows::Forms::Label^ label9;
-	private: System::Windows::Forms::TextBox^ textBox3;
+
 	private: System::Windows::Forms::Label^ label10;
 	private: System::Windows::Forms::TextBox^ textBox4;
 	private: System::Windows::Forms::Label^ label11;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Label^ lblclient;
+	private: System::Windows::Forms::NumericUpDown^ numericUpDown1;
+
+
+
+
+
+
+
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Reference;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Designation;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Couleur;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Prix_HT;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ TVA;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Quantite;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Prix_ttc;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ remise;
+
+
+
+
+
+
+
+
 
 
 	protected:
@@ -85,6 +113,7 @@ namespace Gestionpoo {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle2 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(FicheCommande::typeid));
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label7 = (gcnew System::Windows::Forms::Label());
@@ -100,13 +129,24 @@ namespace Gestionpoo {
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->label9 = (gcnew System::Windows::Forms::Label());
-			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			this->label10 = (gcnew System::Windows::Forms::Label());
 			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
 			this->label11 = (gcnew System::Windows::Forms::Label());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->lblclient = (gcnew System::Windows::Forms::Label());
+			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->Reference = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Designation = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Couleur = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Prix_HT = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->TVA = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Quantite = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Prix_ttc = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->remise = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -161,12 +201,29 @@ namespace Gestionpoo {
 			// 
 			// dataGridView1
 			// 
+			this->dataGridView1->AllowUserToAddRows = false;
+			this->dataGridView1->AllowUserToDeleteRows = false;
+			this->dataGridView1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			dataGridViewCellStyle2->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleCenter;
+			dataGridViewCellStyle2->BackColor = System::Drawing::SystemColors::Control;
+			dataGridViewCellStyle2->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			dataGridViewCellStyle2->ForeColor = System::Drawing::SystemColors::WindowText;
+			dataGridViewCellStyle2->SelectionBackColor = System::Drawing::SystemColors::Highlight;
+			dataGridViewCellStyle2->SelectionForeColor = System::Drawing::SystemColors::HighlightText;
+			dataGridViewCellStyle2->WrapMode = System::Windows::Forms::DataGridViewTriState::True;
+			this->dataGridView1->ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(8) {
+				this->Reference,
+					this->Designation, this->Couleur, this->Prix_HT, this->TVA, this->Quantite, this->Prix_ttc, this->remise
+			});
 			this->dataGridView1->Location = System::Drawing::Point(12, 298);
 			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersVisible = false;
 			this->dataGridView1->RowHeadersWidth = 51;
 			this->dataGridView1->RowTemplate->Height = 24;
-			this->dataGridView1->Size = System::Drawing::Size(984, 296);
+			this->dataGridView1->Size = System::Drawing::Size(984, 271);
 			this->dataGridView1->TabIndex = 25;
 			// 
 			// button9
@@ -235,11 +292,13 @@ namespace Gestionpoo {
 			this->button1->Size = System::Drawing::Size(30, 30);
 			this->button1->TabIndex = 33;
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &FicheCommande::button1_Click);
 			// 
 			// textBox2
 			// 
+			this->textBox2->AutoCompleteMode = System::Windows::Forms::AutoCompleteMode::SuggestAppend;
 			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::None;
-			this->textBox2->Location = System::Drawing::Point(120, 234);
+			this->textBox2->Location = System::Drawing::Point(122, 234);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(125, 20);
 			this->textBox2->TabIndex = 35;
@@ -249,19 +308,11 @@ namespace Gestionpoo {
 			this->label9->AutoSize = true;
 			this->label9->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label9->Location = System::Drawing::Point(29, 234);
+			this->label9->Location = System::Drawing::Point(40, 233);
 			this->label9->Name = L"label9";
 			this->label9->Size = System::Drawing::Size(76, 19);
 			this->label9->TabIndex = 34;
 			this->label9->Text = L"Ref article";
-			// 
-			// textBox3
-			// 
-			this->textBox3->BorderStyle = System::Windows::Forms::BorderStyle::None;
-			this->textBox3->Location = System::Drawing::Point(345, 233);
-			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(125, 20);
-			this->textBox3->TabIndex = 37;
 			// 
 			// label10
 			// 
@@ -281,6 +332,7 @@ namespace Gestionpoo {
 			this->textBox4->Name = L"textBox4";
 			this->textBox4->Size = System::Drawing::Size(125, 20);
 			this->textBox4->TabIndex = 39;
+			this->textBox4->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &FicheCommande::textBox4_KeyPress);
 			// 
 			// label11
 			// 
@@ -317,17 +369,113 @@ namespace Gestionpoo {
 			this->lblclient->Text = L"Client";
 			this->lblclient->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
+			// numericUpDown1
+			// 
+			this->numericUpDown1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->numericUpDown1->Location = System::Drawing::Point(345, 231);
+			this->numericUpDown1->Name = L"numericUpDown1";
+			this->numericUpDown1->Size = System::Drawing::Size(120, 23);
+			this->numericUpDown1->TabIndex = 42;
+			this->numericUpDown1->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			// 
+			// button3
+			// 
+			this->button3->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->button3->FlatAppearance->BorderSize = 0;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button3.Image")));
+			this->button3->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button3->Location = System::Drawing::Point(446, 575);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(116, 38);
+			this->button3->TabIndex = 43;
+			this->button3->Text = L"Valider";
+			this->button3->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button4.BackgroundImage")));
+			this->button4->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->button4->FlatAppearance->BorderSize = 0;
+			this->button4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button4->Location = System::Drawing::Point(966, 12);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(30, 30);
+			this->button4->TabIndex = 44;
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &FicheCommande::button4_Click);
+			// 
+			// Reference
+			// 
+			this->Reference->HeaderText = L"Reference";
+			this->Reference->Name = L"Reference";
+			this->Reference->ReadOnly = true;
+			this->Reference->Width = 140;
+			// 
+			// Designation
+			// 
+			this->Designation->HeaderText = L"Designation";
+			this->Designation->Name = L"Designation";
+			this->Designation->ReadOnly = true;
+			this->Designation->Width = 141;
+			// 
+			// Couleur
+			// 
+			this->Couleur->HeaderText = L"Couleur";
+			this->Couleur->Name = L"Couleur";
+			this->Couleur->ReadOnly = true;
+			this->Couleur->Width = 140;
+			// 
+			// Prix_HT
+			// 
+			this->Prix_HT->HeaderText = L"Prix HT";
+			this->Prix_HT->Name = L"Prix_HT";
+			this->Prix_HT->ReadOnly = true;
+			this->Prix_HT->Width = 141;
+			// 
+			// TVA
+			// 
+			this->TVA->HeaderText = L"TVA";
+			this->TVA->Name = L"TVA";
+			this->TVA->ReadOnly = true;
+			this->TVA->Width = 140;
+			// 
+			// Quantite
+			// 
+			this->Quantite->HeaderText = L"Quantité Commandée";
+			this->Quantite->Name = L"Quantite";
+			this->Quantite->ReadOnly = true;
+			this->Quantite->Width = 141;
+			// 
+			// Prix_ttc
+			// 
+			this->Prix_ttc->HeaderText = L"Prix TTC";
+			this->Prix_ttc->Name = L"Prix_ttc";
+			this->Prix_ttc->ReadOnly = true;
+			this->Prix_ttc->Resizable = System::Windows::Forms::DataGridViewTriState::True;
+			this->Prix_ttc->SortMode = System::Windows::Forms::DataGridViewColumnSortMode::NotSortable;
+			this->Prix_ttc->Width = 140;
+			// 
+			// remise
+			// 
+			this->remise->HeaderText = L"Remise";
+			this->remise->Name = L"remise";
+			this->remise->ReadOnly = true;
+			// 
 			// FicheCommande
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 19);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::AppWorkspace;
 			this->ClientSize = System::Drawing::Size(1008, 625);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->numericUpDown1);
 			this->Controls->Add(this->lblclient);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->textBox4);
 			this->Controls->Add(this->label11);
-			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->label10);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->label9);
@@ -347,8 +495,11 @@ namespace Gestionpoo {
 				static_cast<System::Byte>(0)));
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"FicheCommande";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterParent;
 			this->Text = L"FicheCommande";
+			this->Load += gcnew System::EventHandler(this, &FicheCommande::FicheCommande_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -356,15 +507,21 @@ namespace Gestionpoo {
 #pragma endregion
 		public:
 			void set_client(String^);
+			Composant::Commande^ get_commande();
 			void set_date_livraison(DateTime^);
 			void set_date_emission(DateTime^);
 			void set_adresse_livraison(int);
 			void set_adresse_facturation(int);
 			void set_adresse_list(DataTable^);
 			void set_article_list(DataTable^);
-		private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-			Listearticle^ la = gcnew Listearticle();
-			la->ShowDialog();
-		}
+			System::Collections::Generic::List<Composant::Choisir^>^ get_choix();
+			void set_choix(System::Collections::Generic::List<Composant::Choisir^>^ l);
+		private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e);
+		private: System::Void FicheCommande_Load(System::Object^ sender, System::EventArgs^ e);
+		private: bool existinarticle(String^ f);
+		private: int getartindex(String^ s);
+		private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e);
+		private: System::Void textBox4_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e);
+		private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e);
 };
 }
